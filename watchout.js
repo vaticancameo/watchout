d8852398cce093d83bd33646c1d57396ba11fd38
Angela Wang <angeladanwang@gmail.com>
2015-04-03 15:49:26 -0700

    Draw enemies in a svg element

--- watchout.js
+++ watchout.js
@@ -1,2 +1,24 @@
-// start slingin' some d3 here.
+var svgWidth = window.innerWidth;
+var svgHeight = window.innerHeight;
+
+var svg = d3.select('body').append('svg')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .attr("border", 100)
+    .append('g')
+ //   .attr("transform", "translate(32," + (svgHeight / 2) + ")");
+
+var nodes = d3.range(100).map(function() { return {
+    radius: 15,
+    cx: Math.random() * svgWidth - 50 ,
+    cy: Math.random() * svgHeight - 50 }
+    });
+
+for (var i = 0; i < nodes.length; i++) {
+  svg.append("circle")
+  .attr("cx", nodes[i].cx)
+  .attr("cy", nodes[i].cy)
+  .attr("r", nodes[i].radius)
+  .attr("fill", "blue");
+}
 
